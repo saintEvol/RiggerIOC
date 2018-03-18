016e41c4419a11079c6636de2bb4f50a8c27c913
johnson <casle_death@163.com>
2018-03-18 22:02:19 +0800

    1.增加版权信息

--- src/core/binders/commandBinder/eventCommnadBinder/EventCommandBinder.ts
+++ src/core/binders/commandBinder/eventCommnadBinder/EventCommandBinder.ts
@@ -1,4 +1,20 @@
-/**
+/*
+ * Copyright 2018 Yang Wu.
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *		Unless required by applicable law or agreed to in writing, software
+ *		distributed under the License is distributed on an "AS IS" BASIS,
+ *		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *		See the License for the specific language governing permissions and
+ *		limitations under the License.
+ */
+
+ /**
 * 消息与命令的绑定器
 * 一个消息可以同时绑定多个命令(即一个消息可以导致多个命令的执行)
 * 但一个命令不能同时被绑定到多个消息
