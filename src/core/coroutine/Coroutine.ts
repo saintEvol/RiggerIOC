8d55030f4217ee14b2c0c8c4216f631fac4f6d18
johnson <casle_death@163.com>
2018-12-04 16:37:34 +0800

    waitForSecnods 增加条件判断函数，只要当条件函数或handler返回true时，其才会等待，否则，立马执行

--- src/core/coroutine/Coroutine.ts
+++ src/core/coroutine/Coroutine.ts
@@ -13,37 +13,65 @@
  *		See the License for the specific language governing permissions and
  *		limitations under the License.
  */
-module riggerIOC{
-	export async function startCoroutine(caller:any, method:Function, ...args:any[]){
+module riggerIOC {
+	export interface Action {
+		(): void
+	}
+
+	export async function startCoroutine(caller: any, method: Function, ...args: any[]) {
 		await method.apply(caller, args);
 	}
 
 	/**
 	 * 等下一帧，注意，这里的帧是指浏览器的帧（一般为4MS），而非游戏帧
 	 */
-	export function waitForNextFrame(){
+	export function waitForNextFrame() {
 		return new Promise(resolve => {
 			setTimeout(resolve, 0);
 		})
 	}
 
-	export function waitForSeconds(ms:number){
-		return new Promise(resolve => {
-			setTimeout(resolve, ms);
-		})
+	export function waitForSeconds(ms: number, conditinHandler?: Handler | Action, args = []) {
+		let promise: Promise<any>;
+		if (!conditinHandler) {
+			promise = new Promise(resolve => {
+				setTimeout(resolve, ms);
+			})
+		} else {
+			let ret;
+			if (conditinHandler instanceof Handler) {
+				ret = conditinHandler.runWith(args)
+			}
+			else {
+				ret = conditinHandler()
+			}
+
+			if (ret) {
+				promise = new Promise(resolve => {
+					setTimeout(resolve, ms);
+				})
+			}
+			else {
+				promise = promise = new Promise(resolve => {
+					resolve()
+				})
+			}
+
+		}
+		return promise
 	}
 
 	/**
 	 * 等待命令执行完成
 	 * @param waitable 
 	 */
-	export function waitFor(waitable:IWaitable){
-		return new Promise(resolve =>{
-			if(waitable.isDone()){
+	export function waitFor(waitable: IWaitable) {
+		return new Promise(resolve => {
+			if (waitable.isDone()) {
 				resolve();
 			}
-			else{
-				waitable.setDoneCallback(resolve);				
+			else {
+				waitable.setDoneCallback(resolve);
 			}
 		})
 	}
