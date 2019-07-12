43025763855920360d343e7952b356b14a0af541
johnson <casle_death@163.com>
2019-07-12 18:32:11 +0800

    1. BUG修复

--- gulpTasks/riggerIOCPublishTask.js
+++ gulpTasks/riggerIOCPublishTask.js
@@ -5,7 +5,7 @@ var RiggerPublishUtils = require("./utils/riggerIOCPublishUtils.js");
 var RiggerIOC = require("./utils/riggerIOC.js");
 
 gulp.task("publish", function(){
-    var cmd = 'tsc-plus -p "." --outDir dist/riggerIOC --reorderFiles true --outFile dist/riggerIOC/bin/riggerIOC.min.js ';
+    var cmd = 'tsc-plus -p "./" --outDir dist/riggerIOC --reorderFiles true --outFile dist/riggerIOC/bin/riggerIOC.min.js ';
     exec(cmd, function(error, stdout, stdErr){
         console.log("std out:" + stdout);
         console.log("std err:" + stdErr);
